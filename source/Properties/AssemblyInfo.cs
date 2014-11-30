1b5ff261ad16a724d8d3b776d43afc5e9271a4ed
David Laurie <krimzon@gmail.com>
2014-11-30 10:29:48 +0000

    Fixed assembly version

--- source/Properties/AssemblyInfo.cs
+++ source/Properties/AssemblyInfo.cs
@@ -3,18 +3,18 @@ using System.Runtime.CompilerServices;
 
 // Information about this assembly is defined by the following attributes. 
 // Change them to the values specific to your project.
-[assembly: AssemblyTitle ("KrimZonJsonKerman")]
-[assembly: AssemblyDescription ("")]
+[assembly: AssemblyTitle ("KrimZon Json Kerman")]
+[assembly: AssemblyDescription ("Web Flight Info Display addon for Kerbal Space Program")]
 [assembly: AssemblyConfiguration ("")]
-[assembly: AssemblyCompany ("")]
-[assembly: AssemblyProduct ("")]
-[assembly: AssemblyCopyright ("kdje")]
+[assembly: AssemblyCompany ("Slightly Interactive Technologies")]
+[assembly: AssemblyProduct ("Json Kerman")]
+[assembly: AssemblyCopyright ("Copyright © 2014")]
 [assembly: AssemblyTrademark ("")]
 [assembly: AssemblyCulture ("")]
 // The assembly version has the format "{Major}.{Minor}.{Build}.{Revision}".
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
-[assembly: AssemblyVersion ("1.0.*")]
+[assembly: AssemblyVersion ("0.1.1")]
 // The following attributes are used to specify the signing key for the assembly, 
 // if desired. See the Mono documentation for more information about signing.
 //[assembly: AssemblyDelaySign(false)]
