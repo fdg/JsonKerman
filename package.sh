4f5cdb733f729811d272088bd8943d6a42a8ed14
David Laurie <krimzon@gmail.com>
2014-11-30 09:53:12 +0000

    Updated packaging script

--- package.sh
+++ package.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 rm "JsonKerman.zip"
-zip -r "JsonKerman.zip" "GameData/KrimZon/JsonKerman/JsonKerman.dll" "GameData/KrimZon/JsonKerman/PluginData/"
+zip -r "JsonKerman.zip" "GameData/KrimZon/JsonKerman/JsonKerman.dll" "GameData/KrimZon/JsonKerman/PluginData/" "GameData/KrimZon/README-JsonKerman.txt"
